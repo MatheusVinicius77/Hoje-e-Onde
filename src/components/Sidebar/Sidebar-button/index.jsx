7249e0468b29f961484800ee7f62085d3d54b91f
Matheus Vinicius <matheusviniciusbus7@hotmail.com>
2023-10-13 15:16:12 -0300

    fix: função SidebarButton retorna apenas butão

--- src/components/Sidebar/Sidebar-button/index.jsx
+++ src/components/Sidebar/Sidebar-button/index.jsx
@@ -4,11 +4,9 @@ import styles from './styles.module.css'
 
 export default function SidebarButton({ icone, texto }) {
     return (
-        <div>
-            <button className={`flex transparent ${styles.button}`} type="button">
-                <img src={icone} alt="" />
-                <h1 className='text-5 weight-3'>{texto}</h1>
-            </button>
-        </div >
+        <button className={`flex transparent ${styles.button}`} type="button">
+            <img src={icone} alt="" />
+            <h1 className='text-5 weight-3'>{texto}</h1>
+        </button>
     )
 }
