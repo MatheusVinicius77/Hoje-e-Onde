829d2b2c85f2a95b067b9c521ba885dfea3dec7f
Matheus Vinicius <matheusviniciusbus7@hotmail.com>
2023-10-14 21:21:09 -0300

    feat: adiciona campos restantes do formulário

--- src/pages/NewEvent/index.jsx
+++ src/pages/NewEvent/index.jsx
@@ -1,19 +1,73 @@
 import React from 'react'
 import styles from './styles.module.css'
 import Sidebar from '../../components/Sidebar/index'
-
+import Field from '../../components/FieldsNewEvents/index'
+import EventCategory from '../../components/EventsCategorys/index'
 export default function NewEventPage() {
     return (
         <div className={`${styles.container}`}>
             <div className={`${styles.sidebar}`}>
                 <Sidebar></Sidebar>
             </div>
-            <div className={`flex column align-center  ${styles.content}`}>
+            <div className={`flex column wrap align-center  ${styles.content}`}>
                 <figure className={` weight-3 ${styles.figures}`}>
                     <label htmlFor="imagemInput" className={`text-1 weight-3 ${styles.label}`}>Imagem</label>
                     <input type="file" id="imagemInput" accept="image/*" />
                 </figure>
+                <Field height="44px" text={'Nome do evento'} id='event-name' />
+                <Field height="125px" text={'Descrição'} id='event-desc' />
+                <div className={`flex align-center justify-between ${styles.dateContainer} `}>
+                    <div className={`${styles.firstDate}`}>
+                        <Field width="161px" type='date' height="44px" text={'Data evento'} id='event-date' />
 
+                    </div>
+                    <Field width="119px" type='time' height="44px" text={'Horário inicio'} id='event-start' />
+                    <Field width="119px" type='time' height="44px" text={'Horário fim'} id='event-end' />
+                </div>
+
+
+                <div className={`flex  ${styles.address}`}>
+                    <div className={`${styles.teste}`}>
+                        <Field width="465px" height="39px" text={'Endereço'} id='event-add' />
+                    </div>
+                    <Field width="211.36px" height="39px" text={'Número'} id='event-add-num' />
+                    <Field width="211.36px" height="39px" text={'CEP'} id='event-cep' />
+                </div>
+                <div className={`${styles.categoryContainer}`}>
+                    <h1 className='text-3 weight-3'>Selecione pelo menos duas categorias</h1>
+                    <div className={`flex ${styles.categoryBox}`}>
+                        <div className='flex column justify-evenly'>
+                            <EventCategory categoryName={'Esporte'} />
+                            <EventCategory categoryName={'Esporte'} />
+                            <EventCategory categoryName={'Esporte'} />
+                        </div>
+                        <div className='flex column justify-evenly'>
+                            <EventCategory categoryName={'Educação'} />
+                            <EventCategory categoryName={'Educação'} />
+                            <EventCategory categoryName={'Educação'} />
+                        </div>
+                        <div className='flex column justify-evenly'>
+                            <EventCategory categoryName={'Educação'} />
+                            <EventCategory categoryName={'Educação'} />
+                            <EventCategory categoryName={'Educação'} />
+                        </div>
+                        <div className='flex column justify-evenly'>
+                            <EventCategory categoryName={'Corporativo'} />
+                            <EventCategory categoryName={'Corporativo'} />
+                            <EventCategory categoryName={'Corporativo'} />
+                        </div>
+                        <div className='flex column justify-evenly'>
+                            <EventCategory categoryName={'Balada'} />
+                            <EventCategory categoryName={'Balada'} />
+                            <EventCategory categoryName={'Balada'} />
+                        </div>
+                        <div className='flex column justify-evenly'>
+                            <EventCategory categoryName={'Esporte'} />
+                            <EventCategory categoryName={'Esporte'} />
+                            <EventCategory categoryName={'Esporte'} />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
