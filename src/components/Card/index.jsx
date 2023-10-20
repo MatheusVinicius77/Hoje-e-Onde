5fa9e12427954da30748230a5dd89818a61f83a1
Matheus Vinicius <matheusviniciusbus7@hotmail.com>
2023-10-20 04:40:50 -0300

    fix: refatora card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,33 +5,33 @@ import share from '../../assets/icons/share.svg'
 import save from '../../assets/icons/save.svg'
 import like from '../../assets/icons/favorito.svg'
 
-export default function card({linkImagem,titulo,membros,distancia,hora}){
-    return(
-        
-            <div className={styles["content-card"]}  style={{backgroundImage:`url(${linkImagem})`}}>
-                <div className={styles["image-card"]} style={{backgroundImage:`url(${navWave})`}}>
-                    <div className={styles["linha1"]}>
-                        <div className={styles["iconePerfil"]}>
-                            <img src={linkImagem} alt="" />
-                        </div>
-                        <div className={styles["iconesTop"]}>
-                            <img className={styles["share"]} src={share} alt="" srcset="" />
-                            <img className={styles["save"]} src={save} alt="" srcset="" />
-                        </div>
+export default function Card({ linkImagem, titulo, membros, distancia, hora }) {
+    return (
+
+        <div className={styles["content-card"]} style={{ backgroundImage: `url(${linkImagem})` }}>
+            <div className={styles["image-card"]} style={{ backgroundImage: `url(${navWave})` }}>
+                <div className={styles["linha1"]}>
+                    <div className={styles["iconePerfil"]}>
+                        <img src={linkImagem} alt="" />
                     </div>
-                    <div className={styles["linha2"]}>
-                        <h6>{titulo}</h6>
-                    </div>
-                    <div className={styles["linha3"]}>
-                        <div className={styles["likeContent"]}>
-                            <img src={like} alt="" />
-                            <p>{membros} Membros</p>
-                        </div>
-                        <p>{distancia}Km</p>
-                        <p>{hora}hrs</p>
+                    <div className={styles["iconesTop"]}>
+                        <img className={styles["share"]} src={share} alt="" srcset="" />
+                        <img className={styles["save"]} src={save} alt="" srcset="" />
                     </div>
                 </div>
+                <div className={styles["linha2"]}>
+                    <h6 className="text-1">{titulo}</h6>
+                </div>
+                <div className={styles["linha3"]}>
+                    <div className={styles["likeContent"]}>
+                        <img src={like} alt="" />
+                        <p className="text-6">{membros} Membros</p>
+                    </div>
+                    <p className="text-6">{distancia}Km</p>
+                    <p className="text-6">{hora}hrs</p>
+                </div>
             </div>
-        
+        </div>
+
     );
 }
