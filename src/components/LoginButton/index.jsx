0e8a4162d90c61ff08b495bb39cc300147962b8f
Matheus Vinicius <matheusviniciusbus7@hotmail.com>
2023-10-22 11:36:00 -0300

    fix: muda tipo do botão para submit

--- src/components/LoginButton/index.jsx
+++ src/components/LoginButton/index.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function Button({ style, placeholder }) {
-    return (<button style={style} type="button" className={`${styles.customButton}`}>{placeholder}</button>
-
+    return (<button style={style} type="submit" className={`${styles.customButton}`}>{placeholder}</button>
     );
 }
 export default Button;
