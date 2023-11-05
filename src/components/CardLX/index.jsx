c3cdf76814e06985f5ff54c6820338e4a8454008
Matheus Vinicius <matheusviniciusbus7@hotmail.com>
2023-11-05 10:16:52 -0300

    fix: arrumo largura dos cards grandes

--- src/components/CardLX/index.jsx
+++ src/components/CardLX/index.jsx
@@ -1,14 +1,13 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import styles from "./styles.module.css";
 import share from '../../assets/icons/share.svg'
 import save from '../../assets/icons/save.svg'
 import favorito from '../../assets/icons/favorito.svg'
 import CardModal from "../CardModal";
 //Bootstrap
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-export default function CardLx({fotoIcone,linkImagem}) {
+export default function CardLx({ fotoIcone, linkImagem }) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -17,26 +16,28 @@ export default function CardLx({fotoIcone,linkImagem}) {
     return (
 
         <div className={styles["banner-card"]} style={{ backgroundImage: `url(${linkImagem})` }}>
-            <Button variant="" className={styles.playbt} onClick={handleShow}>
-                    
-            </Button>
-             <section>
-                    <button>
-                        <img src={favorito} alt="" />
-                        <p className="text-7">2 Membros</p>
-                    </button>
-                    
-                    
-                    <button><img src={share} alt="" /></button>
-                    <button><img src={save} alt="" /></button>
-             </section>
+            <div variant="" className={styles.playbt} onClick={handleShow}>
+
+            </div>
+            <section>
+                <button>
+                    <img src={favorito} alt="" />
+                    <p className="text-7">2 Membros</p>
+                </button>
+
+
+                <button><img src={share} alt="" /></button>
+                <button><img src={save} alt="" /></button>
+            </section>
 
             {/* MODAL */}
-             <Modal
+            <Modal
                 show={show}
                 onHide={handleClose}
             >
-                <CardModal fotoIcone={fotoIcone} linkImagem={linkImagem}/>
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <CardModal fotoIcone={fotoIcone} linkImagem={linkImagem} />
             </Modal>
         </div>
 
